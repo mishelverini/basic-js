f1b54b88ff33703e9e2ddaf6e8f638088ace4135
Mikhail Vazhynski <mishelverini@yahoo.com>
2020-03-22 02:13:57 +0300

    init: start dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for(let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      members[i] = members[i].trim();
+      members[i] = members[i][0].toUpperCase();
+    } else {
+      delete members[i];
+    }
+  }
+  
+  return members.sort().join('');
 };
