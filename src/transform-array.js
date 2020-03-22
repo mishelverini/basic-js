439fd9c7772ec7063fe20a5305c27cdbd542e2ae
Mikhail Vazhynski <mishelverini@yahoo.com>
2020-03-22 13:54:48 +0300

    init: start transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) {
+    throw 'Это не массив !';
+  }
+  for (let i = 0; i < arr.length; i++) {
+      if (arr.length == 1) {
+        return [];
+      }
+    // if (arr[i] == 'undefined') {
+    //     arr.slice(i, 1);
+    //     continue;
+    // }
+    let check = arr[i];
+    if (typeof check == 'string' ) {
+      switch(check) {
+        case '--discard-next': arr.splice(++i, 1)
+        arr.splice(i, 1);
+        break;
+        case '--discard-prev': arr.splice(--i, 1)
+        arr.splice(i, 1);
+        break;
+        case '--double-next': arr.splice(++i, 0, arr[i])
+        arr.splice(--i, 1);
+        break;
+        case '--double-prev': arr.splice(i, 1) 
+        arr.splice(i, 0, arr[i]);
+        break;
+        default:
+          arr.splice(i, 1);  
+      }
+    }
+  }
+  return arr.filter(element => element !== undefined);
 };
